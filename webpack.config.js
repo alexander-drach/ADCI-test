f258fca801fcb651408eca5eb320088c3acae4e1
Alexander Drach <alexander-drach@mail.ru>
2020-10-24 15:51:35 +0600

    worked

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -8,10 +10,36 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
+    devServer: {
+        port: 3000,
+        open: true
+    },
     plugins: [
         new HTMLWebpackPlugin({
-            template: './src/index.html'
+            template: './src/index.html',
+            favicon: './src/favicon.ico'
         }),
-        new CleanWebpackPlugin
-    ]
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin(
+            {
+                filename: '[name].css',
+            }            
+        )
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [{loader: MiniCssExtractPlugin.loader}, 'css-loader']
+            },
+            {
+                test: /\.(png|jpg|svg|gif)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.(ttf|woff|woff2|eot)$/,
+                use: ['file-loader']
+            }
+        ]
+    }
 }
